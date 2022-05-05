472be651b16864f8c05a964c84e014386f892a34
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 18:58:15 -0300

    Add files via upload

---
+++ Objetos2.java
@@ -0,0 +1,17 @@
+
+package objetos2;
+
+public class Objetos2 {
+
+    public static void main(String[] args) {
+        Pessoa joao = new Pessoa();
+        joao.nome = "João";
+        joao.apresentar();
+        
+        Pessoa maria = new Pessoa();
+        maria.nome = "Maria";
+        maria.apresentar();
+        
+    }
+    
+}
---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+
+package objetos2;
+
+class Pessoa {
+    String nome;
+    
+    void apresentar(){
+        System.out.println("Olá, meu nome é "+nome);
+        System.out.println("");
+    }
+}
